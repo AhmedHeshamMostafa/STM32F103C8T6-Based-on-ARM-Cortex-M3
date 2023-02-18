e017ce5b5d3f5def8dff42e1eb6854532ee94786
AhmedHeshamMostafa <ahmed.hesham.mostafa7@gmail.com>
2023-02-19 00:37:18 +0200

    Bug fixes for BIT_MATH.h, and updates for STD_TYPES.h

--- Bit Manipulation and Standard Types/BIT_MATH.h
+++ Bit Manipulation and Standard Types/BIT_MATH.h
@@ -1,7 +1,7 @@
 /*********************************************************************************/
 /*             AUTHOR   : Ahmed Hesham Mostafa                                   */
-/*             DATE     : 10 Feb 2023                                            */
-/*             VERSION  : V01                                                    */
+/*             DATE     : 18 Feb 2023                                            */
+/*             VERSION  : V02                                                    */
 /*********************************************************************************/ 
 
 /******* This file contains the function-like macros used for register manipulations in the driver *******/
@@ -23,19 +23,5 @@
 			  
 #define ASSIGN_BIT(reg, bitNo, val)        (reg) =  ((~(1 << (bitNo))) &  (reg)) | ((val) << (bitNo))   //Writes one or zero in a certain bit position in a register.
                                                                                                         /*Preconditions : bitNo is between 0-31 inclusive,
-                                                                                                          and val is either 1 or 0 only.*/  
-#define SET_CLR_REG(reg, val)              (reg) = ( (val) == 1 ) ? 0xFFFFFFFF : 0x00000000             //Precondition : val is either one or zero.
-
-
-#define WRITE_REG(reg, val)                (reg) = (val)
-
-
-#define READ_REG(reg, var)                 (var) = (reg)
-
-
-#define WRITE_REG_MASKED(reg, val, mask)   (reg) = ((mask) & (val))  |  ((reg) & (~(mask)))             /*Takes an 32-bit value(val) a mask and a register(reg), writes the bits of
-                                                                                                         the val at positions corresponding to the 1s positions in the mask.
-																								         Hint : A mask is a 31-bit value of all zeros except some ones at
-																								         certain positions.*/
-
+                                                                                                          and val is either 1 or 0 only.*/
 #endif 
--- Bit Manipulation and Standard Types/STD_TYPES.h
+++ Bit Manipulation and Standard Types/STD_TYPES.h
@@ -1,7 +1,7 @@
 /*********************************************************************************/
 /*             AUTHOR   : Ahmed Hesham Mostafa                                   */
-/*             DATE     : 10 Feb 2023                                            */
-/*             VERSION  : V01                                                    */
+/*             DATE     : 18 Feb 2023                                            */
+/*             VERSION  : V02                                                    */
 /*********************************************************************************/
 
 /******* This file contains the type definitions used to standardize data types in the driver *******/
@@ -11,14 +11,14 @@
 #ifndef STD_TYPES_H_
 #define STD_TYPES_H_
 
-typedef  unsigned char      u8;
-typedef  signed char        s8;
-typedef  unsigned int       u6;
-typedef  signed int         s6;
-typedef  unsigned long      u32;
-typedef  signed long        s32;
-typedef  float              f32;
-typedef  u8                 Bool;                         /*For the data that is either 1 or 0, this unique descriptive
+typedef    unsigned char         u8;
+typedef    signed   char         s8;
+typedef    unsigned short int    u16;
+typedef    signed   short int    s16;
+typedef    unsigned int          u32;
+typedef    signed   int          s32;
+typedef    float                 f32;
+typedef    u8                    Bool;                         /*For the data that is either 1 or 0, this unique descriptive
                                                             data type is preferred.*/
 
 
