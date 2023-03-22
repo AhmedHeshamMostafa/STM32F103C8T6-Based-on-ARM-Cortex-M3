165e041e8548e0159fd58344402693f45f90b809
Ahmed Hesham Mostafa <ahmed.hesham.mostafa7@gmail.com>
2023-03-22 05:22:47 +0200

    3rd Release

--- Bit Manipulation and Standard Types/BIT_MATH.h
+++ Bit Manipulation and Standard Types/BIT_MATH.h
@@ -1,7 +1,7 @@
 /*********************************************************************************/
 /*             AUTHOR   : Ahmed Hesham Mostafa                                   */
-/*             DATE     : 18 Feb 2023                                            */
-/*             VERSION  : V02                                                    */
+/*             DATE     : 18 Mar 2023                                            */
+/*             VERSION  : V03                                                    */
 /*********************************************************************************/ 
 
 /******* This file contains the function-like macros used for register manipulations in the driver *******/
@@ -9,19 +9,24 @@
 #ifndef BIT_MATH_H_
 #define BIT_MATH_H_
 
-#define SET_BIT(reg, bitNo)                (reg) |= (1 << (bitNo))                                      //Precondition : bitNo is between 0-31 inclusive.
+
+/*Precondition : bitNo is between 0-31 inclusive.*/
+#define SET_BIT(reg, bitNo)                ((reg) |= (1 << (bitNo)))
+
+/*Precondition : bitNo is between 0-31 inclusive.*/
+#define CLR_BIT(reg, bitNo)                ((reg) &= (~(1 << (bitNo))))
+
+/*Precondition : bitNo is between 0-31 inclusive.*/
+#define TOGGLE_BIT(reg, bitNo)             ((reg) ^= (1 << (bitNo)))
+
+/*Reads a certain bit in a register and makes it the LSB in val.*/
+/*Precondition : bitNo is between 0-31 inclusive.*/
+#define GET_BIT(reg, bitNo, var)           ((var) =  ((reg) & (1 << (bitNo))) >> (bitNo))
+
+/*Writes one or zero in a certain bit position in a register.*/
+/*Preconditions : bitNo is between 0-31 inclusive,
+and val is either 1 or 0 only.*/
+#define ASSIGN_BIT(reg, bitNo, val)        ((reg) =  ((~(1 << (bitNo))) &  (reg)) | ((val) << (bitNo)))
 
 
-#define CLR_BIT(reg, bitNo)                (reg) &= (~(1 << (bitNo)))                                   //Precondition : bitNo is between 0-31 inclusive.
-
-
-#define TOGGLE_BIT(reg, bitNo)             (reg) ^= (1 << (bitNo))                                      //Precondition : bitNo is between 0-31 inclusive.
-
-
-#define GET_BIT(reg, bitNo, var)           (var) =  ((reg) & (1 << (bitNo))) >> (bitNo)                 //Reads a certain bit in a register and makes it the LSB in val.
-                                                                                                        //Precondition : bitNo is between 0-31 inclusive.
-			  
-#define ASSIGN_BIT(reg, bitNo, val)        (reg) =  ((~(1 << (bitNo))) &  (reg)) | ((val) << (bitNo))   //Writes one or zero in a certain bit position in a register.
-                                                                                                        /*Preconditions : bitNo is between 0-31 inclusive,
-                                                                                                          and val is either 1 or 0 only.*/
 #endif 
