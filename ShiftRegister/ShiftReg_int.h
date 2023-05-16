4b38a72d4ecb08fc4f134c559a6248e2f3bc3580
Ahmed Hesham Mostafa <ahmed.hesham.mostafa7@gmail.com>
2023-05-16 04:34:53 +0200

    1st Release

---
+++ ShiftRegister/ShiftReg_int.h
@@ -0,0 +1,14 @@
+/*********************************************************************************/
+/*             AUTHOR   : Ahmed Hesham Mostafa                                   */
+/*             DATE     : 15 May 2023                                            */
+/*             VERSION  : V01                                                    */
+/*********************************************************************************/
+
+#ifndef HAL_SHIFTREGISTER_SHIFTREG_INT_H_
+#define HAL_SHIFTREGISTER_SHIFTREG_INT_H_
+
+
+
+
+
+#endif /* HAL_SHIFTREGISTER_SHIFTREG_INT_H_ */
